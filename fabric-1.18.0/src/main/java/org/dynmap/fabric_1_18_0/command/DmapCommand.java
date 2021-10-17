eb14e9daf908b4ef54d78724e5ce12fb102866c8
Adam Madsen <adam@ajmadsen.com>
2021-10-17 20:21:54 +0000

    Initial copy of fabric-1.17.1 to more easily see changes

---
+++ fabric-1.18.0/src/main/java/org/dynmap/fabric_1_18_0/command/DmapCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_18_0.command;
+
+import org.dynmap.fabric_1_18_0.DynmapPlugin;
+
+public class DmapCommand extends DynmapCommandExecutor {
+    public DmapCommand(DynmapPlugin p) {
+        super("dmap", p);
+    }
+}
---
+++ fabric-1.18.0/src/main/java/org/dynmap/fabric_1_18_0/command/DynmapExpCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_18_0.command;
+
+import org.dynmap.fabric_1_18_0.DynmapPlugin;
+
+public class DynmapExpCommand extends DynmapCommandExecutor {
+    public DynmapExpCommand(DynmapPlugin p) {
+        super("dynmapexp", p);
+    }
+}
